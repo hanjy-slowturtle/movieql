c8cf8c1f7c853f198e07c92bdda3f09aa490bf52
hanjy-slowturtle <h920915@gmail.com>
2021-04-19 23:07:19 +0900

    ~ #0.11 Delete Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,19 +1,49 @@
-export const people = [
+let movies = [
     {
-        id: "0",
-        name: "hanjy",
-        age: 30,
-        gender: "male"
+        id: 0,
+        name: "I'm Legends",
+        score: 80
     },
     {
-        id: "1",
-        name: "hancy",
-        age: 29,
-        gender: "male"
+        id: 1,
+        name: "Iron man",
+        score: 90
+    },
+    {
+        id: 2,
+        name: "Go home",
+        score: 95
+    },
+    {
+        id: 3,
+        name: "Tazza",
+        score: 77
     }
 ];
 
-export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
-    return filteredPeople[0];
-}
+export const getMovies = () => movies;
+
+export const getById = (id) => {
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0];
+};
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: movies.length + 1,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+};
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+};
